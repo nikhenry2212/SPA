eb496a18202322f0caeb5e65a9993511b20ecc97
Nikolas <nikolaspg@hotmail.com>
2021-02-10 17:03:53 -0300

    :package: commit One

---
+++ src/components/Contato/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Contato() {
+  return (
+    <div className="home" id="contato">
+        <h1>Entre em contato</h1>
+        <h2>Tel: (xx) 9 9999-9999</h2>
+        <h3>Rua: Alguma, endereço</h3>
+    </div>
+  );
+}
+
+export default Contato;
---
+++ src/components/Sobre/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Sobre() {
+  return (
+    <div className="sobre" id="sobre">
+        <h1>Sobre</h1>
+        <h2>Sobre React Js</h2>
+        <h3>Nossa História</h3>
+    </div>
+  );
+}
+
+export default Sobre;
